f3054008fdaf82abd3a8d02a89ffd65f281306f7
Efithor <Efithor@gmail.com>
2018-03-06 12:24:47 -0600

    Biome placement

--- js/warEcMain.js
+++ js/warEcMain.js
@@ -119,7 +119,7 @@ function createRegions(){
   var regionArray = [];
   regionArray = createHexagonalGrid(20,25,20,50,50);
   setupLand(regionArray);
-  setupRegionBiomes(regionArray);
+  setupRegionBiomes(regionArray, 0.25,0.25,0.25);
 }
 //Given a certain radius, quantity, and origin, create a hexagonal grid.
 function createHexagonalGrid(radius,xCount,yCount,xOrig,yOrig){
@@ -221,9 +221,131 @@ function setupLand(rArray){
       }
     }
   }
+
+  //Ensure the landmasses we just generated are valid.
+  if(!checkLandmass(rArray,.35)){
+    createRegions();
+  }else{
+    return;
+  }
 }
+//Given a region array, verify if there's enough land.
+function checkLandmass(rArray,minPercentNeeded){
+  var totalTiles = getTotalTiles(rArray);
+  var landTiles = getTotalLandTiles(rArray);
+  if(landTiles/totalTiles >= minPercentNeeded){
+    return true;
+  }else{
+    return false;
+  }
+}
+
 //Given a region array, assign biomes.
-function setupRegionBiomes(regionArray){
+function setupRegionBiomes(regionArray, mountainPercent, forestPercent, desertPercent){
+  //Add plains, hills, cities, towns, mountains, tundra, deserts.
+  //Create geographic features
+  placeBiomeType(regionArray,'mountain',mountainPercent); //Mountains
+  placeBiomeType(regionArray,'forest',forestPercent); //Forests
+  placeBiomeType(regionArray,'desert',desertPercent); //Deserts
+  //Remaining tiles are plains
+  for(var x=0;x<regionArray.length;x++){
+    for(var y=0;y<regionArray[x].length;y++){
+      if(regionArray[x][y].isLand && regionArray[x][y].biome === undefined){
+        regionArray[x][y].biome = 'plain';
+        regionArray[x][y].fillColor = '#7eb563';
+      }
+    }
+  }
+  //Divide the land into temp zones
+  //1/6 and 6/6: Artic
+  //Plains -> Tundra
+  //Desert -> Tundra
+  //Forest -> Boreal Forest
+  //Mountain ->  Snowy Mount
+
+  //2/6 and 5/6: Temperate
+  //Plains -> Grassland
+  //Desert -> Desert
+  //Forest -> Forest
+  //Mountain ->  Snow-Capped Mount
+
+  //Middle third: Tropical
+  //Plains -> Swamp
+  //Desert -> Desert
+  //Forest -> Jungle
+  //Mountain -> Mount
+
+  //Place a number of towns relative to the size of the map.
+  //Biased towards coasts, temperate, and tropical areas.
+
+  //Upgrade a percentage of the towns to cities
+  //Biased towards cities that have more towns nearby.
+
+}
+//Choose a random land square and put a biomeType on it.
+//Either grab a random adjacent squre (80% chance?) and put a biomeType
+//there, or choose a new nonbiomeType square.
+//Do this until a certain percentage of biomeType have been created.
+//Will not override biomes already created.
+function placeBiomeType(rArray, biomeType, maxPercent){
+  //Create an array of valid land.
+  var validLandArray = [];
+  var biomeCount = 0;
+  var totalTiles = getTotalLandTiles(rArray);
+
+  for(var x=0;x<rArray.length;x++){
+    for(var y=0;y<rArray[x].length;y++){
+      if(rArray[x][y].isLand && rArray[x][y].biome === undefined){
+        validLandArray.push(rArray[x][y]);
+      }
+    }
+  }
+  //Place biomes. Biased towards placing them near like biomes.
+  var lastPlaced = undefined;
+  while(biomeCount/totalTiles < maxPercent){
+    //If lastPlaced != undefined, have there be an 80% chance of placing another
+    //biomeType next to it.
+    if(lastPlaced!=undefined && Math.random()<=0.8){
+      var adjArray = getAdjacentHexes(rArray,lastPlaced);
+      //Determine which tiles in the adjArray are valid
+      var validAdjArray = [];
+      for(var i=0;i<adjArray.length;i++){
+        if(doesXArrayContainYElement(validLandArray,adjArray[i])){
+          validAdjArray.push(adjArray[i]);
+          //Special case where there arn't any valid tiles.
+          if(validAdjArray.length === 0){
+            //Choose a random plot of valid land. Put a biome on it.
+            var validID = Math.floor(Math.random()*validLandArray.length);
+            validLandArray[validID].biome = biomeType;
+            lastPlaced=validLandArray[validID];
+            //remove this element from the valid land array.
+          }else{
+            //Otherwise, choose one from the validAdjArray.
+            var validAdjID = Math.floor(Math.random()*validAdjArray.length)
+            validAdjArray[validAdjID].biome = biomeType;
+            lastPlaced = validAdjArray[validAdjID];
+            //remove this tile from the valid land array.
+          }
+        }
+      }
+    }else{
+      //Choose a random plot of valid land. Put a mountain on it.
+      var validID = Math.floor(Math.random()*validLandArray.length);
+      validLandArray[validID].biome = biomeType;
+      lastPlaced=validLandArray[validID];
+      //remove this place from the valid land array.
+    }
+    if(biomeType === 'mountain'){
+      validLandArray[validID].fillColor = 'grey';
+    }
+    if(biomeType === 'forest'){
+      validLandArray[validID].fillColor = 'green';
+    }
+    if(biomeType === 'desert'){
+      validLandArray[validID].fillColor = '#c4b850';
+    }
+    biomeCount++;
+  }
 
 }
 
@@ -509,4 +631,26 @@ function doesXArrayContainYElement(x,y){
     }
   }
   return false;
+}
+//Given a region array, calculat the total tiles.
+function getTotalTiles(rArray){
+  var totalTiles = 0;
+  for(var x=0;x<rArray.length;x++){
+    for(var y=0;y<rArray[x].length;y++){
+      totalTiles++;
+    }
+  }
+  return totalTiles;
+}
+
+function getTotalLandTiles(rArray){
+  var landTiles = 0;
+  for(var x=0;x<rArray.length;x++){
+    for(var y=0;y<rArray[x].length;y++){
+      if(rArray[x][y].isLand){
+        landTiles++;
+      }
+    }
+  }
+  return landTiles;
 }
